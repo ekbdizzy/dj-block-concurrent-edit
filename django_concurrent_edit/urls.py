a0e847b1ba94d709b42fee8e5bb34c8ed9fc49ff
Aleksey Koshkin <ekbdizzy@yandex.ru>
2021-09-27 14:30:54 +0300

    Add static settings

--- django_concurrent_edit/urls.py
+++ django_concurrent_edit/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('post.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
