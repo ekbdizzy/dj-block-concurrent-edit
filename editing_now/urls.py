fd323621ca4d92d5f92a933cabf8e376a7220c2c
Aleksey Koshkin <ekbdizzy@yandex.ru>
2021-09-28 15:46:01 +0300

    Add time tracker

--- editing_now/urls.py
+++ editing_now/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import is_editing_now
+from .views import is_editing_now, update_editing_now_model
 
 app_name = 'editing_now'
 
 urlpatterns = [
-    path('check/<str:model_name>/', is_editing_now)
+    path('check/<str:model_name>/', is_editing_now),
+    path('update/<str:model_name>/', update_editing_now_model),
 ]
--- editing_now/views.py
+++ editing_now/views.py
@@ -1,10 +1,20 @@
+import datetime
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from editing_now.models import EditingNow
+from django.utils import timezone
 
 
 def is_editing_now(request, model_name):
     editing_now_object = EditingNow.objects.get(model_name=model_name)
     print(editing_now_object)
-
     return JsonResponse({'model': editing_now_object.model_name})
+
+
+def update_editing_now_model(request, model_name):
+    if request.method == 'UPDATE':
+        EditingNow.objects.filter(model_name=model_name).update(
+            is_updated=timezone.now())
+        return JsonResponse({'model': "updated"})
+    return JsonResponse({'error': 'method is not valid'})
